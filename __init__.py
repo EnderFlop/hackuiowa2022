c69ed1b4266cea329a0c05d2f11299d9355380b3
agbarloon <agbarloon@uiowa.edu>
2022-10-16 09:41:26 -0500

    add /quiz

--- __init__.py
+++ __init__.py
@@ -18,15 +18,11 @@ class Generator:
         self.model = model
         self.max_tokens = max_tokens
     
-    def create_prompt(self):
-        return f"Summarize this in at least {self.min_sentences} sentences and at most {self.max_sentences} sentences for a {self.grade_level}-grade student:\n\nText: "
-        
-    def append_text(self, text):
-        return text + "\n\nSummary: "
+    def create_summary_prompt(self, text):
+        return f"Summarize this in at least {self.min_sentences} sentences and at most {self.max_sentences} sentences for a {self.grade_level}-grade student:\n\nText: {text}\n\nSummary: "
 
-    def generate(self, text):
-        prompt = self.create_prompt()
-        prompt += self.append_text(text)
+    def generate_summary(self, text):
+        prompt = self.create_summary_prompt(text)
         completion = openai.Completion.create(  engine = self.model, 
                                                 temperature = self.temperature, 
                                                 prompt = prompt, 
@@ -36,16 +32,34 @@ class Generator:
         completion_text = completion.choices[0].text
         return completion_text
 
+    def generate_quiz(self, text):
+        prompt = f"Make a three question, college-level, multiple choice quiz:\n\n{text}"
+        completion = openai.Completion.create(  engine = self.model, 
+                                                temperature = self.temperature, 
+                                                prompt = prompt, 
+                                                max_tokens = self.max_tokens,
+                                                frequency_penalty = 1.0
+                                                )
+        completion_text = completion.choices[0].text
+        return completion_text
+
+    def generate_answer(self, text):
+        prompt = f"Choose the correct answer:\n\n{text}"
+
 class Summary:
     def __init__(self, title, body, url, generator):
         self.title = title
         self.body = body
         self.url = url
         self.generator = generator
-        self.summarized_body = self.generator.generate(self.body)
+        self.summarized_body = self.generator.generate_summary(self.body)
+        self.quiz = self.generator.generate_quiz(self.body)
     
     def print_summarized_text(self):
         print(self.summarized_body)
+    
+    def generate_answers(self):
+        self.answers = self.generator.generate_answer(self.quiz)
 
 #text-davinci-002, temp = 0.8
 
@@ -69,7 +83,6 @@ def create_app(test_config=None):
     def create_summary():
         data = request.json
         summaries.append(Summary(data["title"], data["body"], data["url"], Generator(0.8, 12, 5, 10, "text-davinci-002", 300)))
-        print(summaries)
         return jsonify(success=True)
     
     @app.post('/clear_summaries')
@@ -88,6 +101,14 @@ def create_app(test_config=None):
     def total_summary():
         s = summarize_summaries(summaries)
         response = jsonify({"title": s.title, "summarized_body": s.summarized_body, "body": s.body, "url": s.url})
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return response
+
+    @app.get('/quiz')
+    def quiz():
+        all_quizes = [s.quiz for s in summaries]
+        all_answers = [s.generate_answers() for s in summaries]
+        response = jsonify(zip(all_quizes, all_answers))
         response.headers.add("Access-Control-Allow-Origin", "*")
         return response
 
